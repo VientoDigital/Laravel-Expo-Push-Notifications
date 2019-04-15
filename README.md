04e3a765574fe73efb92d6a9ef7b9b32fad81c67
Victor Yoalli <victoryoalli@gmail.com>
2019-04-14 21:58:30 -0500

    readme

---
+++ README.md
@@ -0,0 +1,59 @@
+# Laravel Expo Push Notifications
+
+A Laravel Channel for sending push notifications to an Expo React Native App.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Laravel 5.7+
+PHP 7+
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+composer require vientodigital/laravel-expo-push-notification
+```
+
+And repeat
+
+```
+
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+TODO
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+
+## Authors
+
+* **Victor Yoalli Dominguez** - *Initial work* - [VientoDigital](https://github.com/VientoDigital)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/Channels/ExpoPushNotificationChannel.php
+++ src/Channels/ExpoPushNotificationChannel.php
@@ -4,7 +4,6 @@ namespace VientoDigital\LaravelExpoPushNotifications\Channels;
 
 use Illuminate\Notifications\Notification;
 use GuzzleHttp\Client as HttpClient;
-use Illuminate\Support\Facades\Log;
 use VientoDigital\LaravelExpoPushNotifications\ExpoPushNotifiable;
 
 class ExpoPushNotificationChannel
@@ -23,23 +22,19 @@ class ExpoPushNotificationChannel
         if (!method_exists($notification, 'toExpo')) {
             throw new RuntimeException('Notification is missing toExpo method.');
         }
-        // dump($notifiable->tokens());
         $data = $notification->toExpo($notifiable);
-        // dump($data);
-        // return;
         $this->sentTokens($notifiable, $notification, $data);
         ;
         if (!empty($notifiable->tokens()) && is_array($notifiable->tokens())) {
         }
     }
 
-    private function sentTokens(ExpoPushNotifiable $notifiable, Notification $notification, array $data)
+    private function sentTokens(ExpoPushNotifiable $notifiable, Notification $notification, $data)
     {
         $tokens = collect($notifiable->tokens());
         $tokens->each(function ($to, $key) use ($notifiable, $notification,$data) {
             $data = $this->buildMessage($notification->toExpo($notifiable));
             $data['to'] = $to;
-            Log::info(['data' => $data]);
             $this->http->request('POST', $this->url, ['json' => $data]);
         });
     }
@@ -52,7 +47,7 @@ class ExpoPushNotificationChannel
             'subtitle' => $data->subtitle,
             'priority' => $data->priority,
             'sound' => $data->sound,
-            'badgeCount' => $data->badgeCount,
+            'badge' => $data->badgeCount,
         ];
         return $result;
     }
